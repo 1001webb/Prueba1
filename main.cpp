d91478eef7e4c472de5db46d862b2a7ccd678028
1001webb <69720765+1001webb@users.noreply.github.com>
2020-08-15 09:52:03 -0600

    Verificacion de menu
    Master verifico menu de Webb

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char** argv) {
 	cout<<"Hola Mundo."<<endl;
 	cout<<"Prueba Master"<<endl;
 	cout<<"Agrege un menu"<<endl;
+	cout<<"Master verifica el menu de Bienvenida."<<endl;
 	
 	return 0;
 }
